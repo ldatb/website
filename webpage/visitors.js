66375209bc2f58b306fdc5131f4fd8af1c6d06ac
Lucas de Ataides <lucas.deataidesbarreto@windriver.com>
2023-02-06 11:42:47 -0300

    Fix visitor counter not showing up

--- webpage/visitors.js
+++ webpage/visitors.js
@@ -1,16 +1,8 @@
 const getVisitors = async () => {
-    const request = await fetch('api.ldatb.com/v1/visitors', {
-        method: "GET",
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
-            "Access-Control-Allow-Headers": "Content-Type",
-        }
-    });
-    const response = await request.json();
-    const json = JSON.parse(response.body);
+    const request = await fetch("/api/v1/visitors")
+    const data = await request.json();
+    const json = JSON.parse(data.body);
     const innerHTML = "<span>Users that have visited this page: " + json["visitors"] + "</span>";
     document.getElementById("visitor-counter").innerHTML += innerHTML
-    console.log(json["visitors"])
 }
 getVisitors()
